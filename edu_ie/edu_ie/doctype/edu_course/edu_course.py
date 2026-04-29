57a93d37db7adfd771e6279b462aa74b44b3eefb
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-04-29 10:17:33 +0700

    Update

---
+++ edu_ie/edu_ie/doctype/edu_course/edu_course.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, IE and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EDUCourse(Document):
+	pass
